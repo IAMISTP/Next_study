81ef449b90dacfd71e3b3d6d1af2ca48fedda04b
박혜민 <hyeminpak@gmail.com>
2023-07-13 22:51:51 +0900

    헤더 view 작업완료

--- app/page.js
+++ app/page.js
@@ -1,3 +1,45 @@
+import { GrFavorite, GrCart, GrLocation } from "react-icons/gr";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <div className="aside_menu">
+          <div>회원가입</div>
+          <div>로그인</div>
+          <div>고객센터</div>
+        </div>
+        <div className="main_menu">
+          <div className="logo_warp">
+            <span>fresh</span>
+          </div>
+          <div className="search_warp">
+            <input />
+          </div>
+          <div className="icon_warp">
+            <span>
+              <GrLocation />
+            </span>
+            <span>
+              <GrFavorite />
+              <span>
+                <GrCart />
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="sub_menu_warp">
+          <div>카테고리</div>
+          <ul>
+            <li>신상품</li>
+            <li>베스트</li>
+            <li>특가/혜택</li>
+          </ul>
+          <div>샛별/배송안내</div>
+        </div>
+      </header>
+      <h4 className="title">애플 후레쉬</h4>
+      <p className="title_sub">by dev kim</p>
+    </div>
+  );
 }
